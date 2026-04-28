299c96ecfffd6288b13412414c1aeaf78d381d36
MiracleOffordile23 <chifurumnanya330@gmail.com>
2026-04-28 22:53:20 +0100

    first commit

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ function updateDisplay() {
     document.getElementById('display').value = calculator;
 }
 
-
-
 function deleteLast() {
     calculator = calculator.slice(0, -1);
     updateDisplay();
